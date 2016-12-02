2e5c62bd92588cb5e948234919bc0ebc8312fc91
Youen Chéné <youen@MacBook-Pro-de-Youen-3.local>
2016-12-02 22:24:29 +0100

    feat: start of exercice

--- tests.js
+++ tests.js
@@ -7,12 +7,4 @@ describe('Main Test Suite', function(){
     expect(calculator1(1,1)).to.equal(2);
   })
 
-  it("1 and 2 should give 9", function(){
-    expect(calculator1(1,2)).to.equal(9);
-  })
-
-  it("5 and 6 should give 25 937 424 601", function(){
-    expect(calculator1(5,6)).to.equal(25937424601);
-  })
-
 });
